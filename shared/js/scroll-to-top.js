44c56a6ecadb2cc8d6012f2e92e7ac80272d381b
Kuti1997 <kutigolberg@gmail.com>
2018-06-12 12:49:52 +0300

    Fix Things.

--- shared/js/scroll-to-top.js
+++ shared/js/scroll-to-top.js
@@ -1,8 +1,3 @@
-/* Unoriginal JS - START
- Taken from: https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
- */
-
-// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -13,12 +8,7 @@ function scrollFunction() {
     }
 }
 
-// When the user clicks on the button, scroll to the top of the document
 function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
-
-/* Unoriginal JS - START
- Taken from: https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
- */
